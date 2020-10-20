c571ee3063d1095cafc7bb0c84a14a496febb42b
Monika Misiewicz <zieba.monika@wp.pl>
2020-10-20 20:54:20 +0200

    first commit

---
+++ src/test/java/pl/monikamisiewicz/garden/GardenApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.monikamisiewicz.garden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
